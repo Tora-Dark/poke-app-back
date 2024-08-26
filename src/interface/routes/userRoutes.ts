f004a10dd2408fdd75f83e652d151ecc7e18aeaf
Daniel Glez <143753934+daniel-glez-ntsprint@users.noreply.github.com>
2024-08-25 21:35:41 -0400

    [ADD] Create templates for plop generator

---
+++ src/interface/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { UserController } from '@interface/controllers/UserController';
+import { UserService } from '@infrastructure/services/UserService';
+
+const router = Router();
+const service = new UserService(); 
+const controller = new UserController(service);
+
+router.get('/', (req, res) => controller.find(req, res));
+
+export { router as userRoutes };
